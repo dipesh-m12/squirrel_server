411d63c08feaab05e5546db6783741ea27e881e1
Dipesh <mavinash423@gmail.com>
2025-04-08 18:26:14 +0530

    server

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "*", // Replace with your specific origin if needed
+    origin: true, // Replace with your specific origin if needed
     credentials: true,
   })
 );
